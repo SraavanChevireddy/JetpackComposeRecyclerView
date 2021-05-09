aab86ba07c440c4461af44a992806dd767c3ed81
Sraavan Chevireddy <schevireddy@saventech.com>
2021-05-09 22:00:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/jetpackcomposerecyclerview/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.jetpackcomposerecyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
